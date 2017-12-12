a2a59104ef12f3397c5a316536fcde5f6db44454
ArminderK <arminder.khinda@yahoo.com>
2017-12-12 11:14:10 -0800

    Added the files and got cipher algorithm to work. TODO: Fix front-end breaking and add usernames next to message

---
+++ cipher.js
@@ -0,0 +1,9 @@
+
+
+
+
+document.getElementById("submitted").onclick = function() { cipherAlglorithm(); }
+
+var randomTopic = randomTopic();
+$('#topic').text("Topic of the Day: ", randomTopic);
+
---
+++ server.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+users = [];
+connections = [];
+
+server.listen(process.env.PORT || 3000);
+console.log('Server running');
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', function(socket) {
+  connections.push(socket);
+  console.log("Connected: %s sockets connected", connections.length);
+  
+  // Disconnect
+  socket.on('disconnect', function(data) {
+    users.splice(users.indexOf(socket.username), 1);
+    updateUserNames();
+    connections.splice(connections.indexOf(socket), 1);
+    console.log('Disconnected: %s sockets connected', connections.length);
+  });
+  
+  // Message
+  socket.on('message', function(data) {
+    io.sockets.emit('new message', {'msg': data});
+  });
+  
+  // New User Has Joined
+  socket.on('new user', function(data, callback) {
+    callback(true);
+    socket.username = data;
+    users.push(socket.username);
+    updateUserNames();
+  });
+  
+  function updateUserNames() {
+    io.sockets.emit('get users', users);
+  }
+  
+});
